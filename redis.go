f16779bc5e565935c34d7df47b4550d3ea0211db
Ludovic Fauvet <etix@videolan.org>
2015-04-21 15:25:33 +0200

    Fix a race condition in redis logging

--- redis.go
+++ redis.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/garyburd/redigo/redis"
+	"sync"
 	"time"
 )
 
@@ -20,10 +21,11 @@ var (
 )
 
 type redisobj struct {
-	pool        *redis.Pool
-	pubsub      *Pubsub
-	failure     bool
-	knownMaster string
+	pool         *redis.Pool
+	pubsub       *Pubsub
+	failure      bool
+	failureState sync.RWMutex
+	knownMaster  string
 }
 
 func NewRedis() *redisobj {
@@ -121,7 +123,7 @@ func (r *redisobj) connect() (redis.Conn, error) {
 
 			// Close the connection to the sentinel
 			c.Close()
-			r.failure = false
+			r.setFailureState(false)
 
 			r.printConnectedMaster(masterhost)
 			return cm, nil
@@ -140,11 +142,11 @@ single:
 		if len(sentinels) == 0 {
 			log.Error("No redis master available")
 		}
-		r.failure = true
+		r.setFailureState(true)
 		return nil, errUnreachable
 	}
 
-	if r.failure == false {
+	if r.getFailureState() == false {
 		log.Warning("No redis master available, trying using the configured RedisAddress as fallback")
 	}
 
@@ -159,15 +161,15 @@ single:
 	role, err := r.askRole(c)
 	if err != nil {
 		r.logError("Redis master: %s", err.Error())
-		r.failure = true
+		r.setFailureState(true)
 		return nil, errUnreachable
 	}
 	if role != "master" {
 		r.logError("Redis master: %s is not a master but a %s", GetConfig().RedisAddress, role)
-		r.failure = true
+		r.setFailureState(true)
 		return nil, errUnreachable
 	}
-	r.failure = false
+	r.setFailureState(false)
 	r.printConnectedMaster(GetConfig().RedisAddress)
 	return c, err
 
@@ -191,7 +193,7 @@ func (r *redisobj) auth(c redis.Conn) (err error) {
 }
 
 func (r *redisobj) logError(format string, args ...interface{}) {
-	if r.failure {
+	if r.getFailureState() == true {
 		log.Debug(format, args...)
 	} else {
 		log.Error(format, args...)
@@ -205,6 +207,18 @@ func (r *redisobj) printConnectedMaster(address string) {
 	} else {
 		log.Debug("Connected to redis master %s", address)
 	}
+}
+
+func (r *redisobj) setFailureState(failure bool) {
+	r.failureState.Lock()
+	r.failure = failure
+	r.failureState.Unlock()
+}
+
+func (r *redisobj) getFailureState() bool {
+	r.failureState.RLock()
+	defer r.failureState.RUnlock()
+	return r.failure
 }
 
 // RedisIsLoading returns true if the error is of type LOADING
