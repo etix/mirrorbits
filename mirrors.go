etix/mirrorbits

commit 048833def0d7c2d1dae2d3bea1c2e6ac77e9a538

mirrorlist: show <1% for mirrors with a weight less than 1

mirrors.go
@@ -36,7 +36,7 @@ type Mirror struct {
 	Distance           float32  `redis:"-" yaml:"-"`
 	CountryFields      []string `redis:"-" json:"-" yaml:"-"`
 	Filepath           string   `redis:"-" json:"-" yaml:"-"`
-	Weight             int      `redis:"-" json:"-" yaml:"-"`
+	Weight             string   `redis:"-" json:"-" yaml:"-"`
 	ComputedScore      int      `redis:"-" yaml:"-"`
 	LastSync           int64    `redis:"lastSync" yaml:"-"`
 	LastSuccessfulSync int64    `redis:"lastSuccessfulSync" yaml:"-"`

selection.go
@@ -7,6 +7,7 @@ import (
 	"math"
 	"math/rand"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -189,7 +190,10 @@ func (h DefaultEngine) Selection(ctx *Context, cache *Cache, fileInfo *FileInfo,
 			}
 			for _, m := range mirrors {
 				if m.ID == id {
-					m.Weight = int(float64(weights[id])*100/float64(totalScore) + 0.5)
+					m.Weight = strconv.FormatFloat(float64(weights[id])*100/float64(totalScore), 'f', 0, 32)
+					if m.Weight == "0" {
+						m.Weight = "<1"
+					}
 					weightedMirrors[i] = m
 					break
 				}
@@ -201,7 +205,7 @@ func (h DefaultEngine) Selection(ctx *Context, cache *Cache, fileInfo *FileInfo,
 		// Replace the head of the list by its reordered counterpart
 		mirrors = append(weightedMirrors, mirrors[selected:]...)
 	} else if selected == 1 && len(mirrors) > 0 {
-		mirrors[0].Weight = 100
+		mirrors[0].Weight = "100"
 	}
 	return
 }

utils.go
@@ -159,7 +159,7 @@ func getMirrorMapUrl(mirrors Mirrors, clientInfo GeoIPRec) string {
 				mirror.Latitude, mirror.Longitude))
 		}
 		color := "blue"
-		if mirror.Weight > 0 {
+		if len(mirror.Weight) > 0 {
 			color = "green"
 		}
 		buffer.WriteString(fmt.Sprintf("&markers=color:%s|label:%d|%f,%f", color, count, mirror.Latitude, mirror.Longitude))

commit 0b3afdf952271a237ab07c1d9204bd63189bad80

templates: export the weight as float instead of string (fixes #21)
The formatting is now the responsability of the templates instead of the core.
The issue was introduced in 048833d.

mirrors.go
@@ -36,7 +36,7 @@ type Mirror struct {
 	Distance           float32  `redis:"-" yaml:"-"`
 	CountryFields      []string `redis:"-" json:"-" yaml:"-"`
 	Filepath           string   `redis:"-" json:"-" yaml:"-"`
-	Weight             string   `redis:"-" json:"-" yaml:"-"`
+	Weight             float32  `redis:"-" json:"-" yaml:"-"`
 	ComputedScore      int      `redis:"-" yaml:"-"`
 	LastSync           int64    `redis:"lastSync" yaml:"-"`
 	LastSuccessfulSync int64    `redis:"lastSuccessfulSync" yaml:"-"`

selection.go
@@ -7,7 +7,6 @@ import (
 	"math"
 	"math/rand"
 	"sort"
-	"strconv"
 	"strings"
 )
 
@@ -190,10 +189,7 @@ func (h DefaultEngine) Selection(ctx *Context, cache *Cache, fileInfo *FileInfo,
 			}
 			for _, m := range mirrors {
 				if m.ID == id {
-					m.Weight = strconv.FormatFloat(float64(weights[id])*100/float64(totalScore), 'f', 0, 32)
-					if m.Weight == "0" {
-						m.Weight = "<1"
-					}
+					m.Weight = float32(float64(weights[id]) * 100 / float64(totalScore))
 					weightedMirrors[i] = m
 					break
 				}
@@ -205,7 +201,7 @@ func (h DefaultEngine) Selection(ctx *Context, cache *Cache, fileInfo *FileInfo,
 		// Replace the head of the list by its reordered counterpart
 		mirrors = append(weightedMirrors, mirrors[selected:]...)
 	} else if selected == 1 && len(mirrors) > 0 {
-		mirrors[0].Weight = "100"
+		mirrors[0].Weight = 100
 	}
 	return
 }

templates/mirrorlist.html
@@ -75,7 +75,7 @@
     {{end}}
     {{range $i, $v := .MirrorList}}
     <tr{{if not $v.Weight}} style="color: grey;"{{end}}>
-            <td style="text-align:right">{{add $i 1}}.</td><td style="text-align:left;"><a href="{{$v.HttpURL}}{{$.FileInfo.Path}}">{{$v.HttpURL}}</a></td><td>{{$v.CountryCodes}}</td><td>{{$v.ContinentCode}}</td><td style="text-align: right">{{printf "%.2f" $v.Distance}}Km</td><td>{{if $v.Weight}}{{$v.Weight}}%{{else}}N/A{{end}}</td><td>{{if $v.FileInfo}}{{$v.FileInfo.Size}}{{end}}</td>
+            <td style="text-align:right">{{add $i 1}}.</td><td style="text-align:left;"><a href="{{$v.HttpURL}}{{$.FileInfo.Path}}">{{$v.HttpURL}}</a></td><td>{{$v.CountryCodes}}</td><td>{{$v.ContinentCode}}</td><td style="text-align: right">{{printf "%.2f" $v.Distance}}Km</td><td>{{if $v.Weight}}{{if ge $v.Weight 1.0}}{{printf "%.0f" $v.Weight}}{{else}}<1{{end}}%{{else}}N/A{{end}}</td><td>{{if $v.FileInfo}}{{$v.FileInfo.Size}}{{end}}</td>
         </tr>
     {{else}}
     <i>No mirrors for this file</i>

utils.go
@@ -159,7 +159,7 @@ func getMirrorMapUrl(mirrors Mirrors, clientInfo GeoIPRec) string {
 				mirror.Latitude, mirror.Longitude))
 		}
 		color := "blue"
-		if len(mirror.Weight) > 0 {
+		if mirror.Weight > 0 {
 			color = "green"
 		}
 		buffer.WriteString(fmt.Sprintf("&markers=color:%s|label:%d|%f,%f", color, count, mirror.Latitude, mirror.Longitude))