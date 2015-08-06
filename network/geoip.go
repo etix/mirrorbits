4e277050f42d0bbc019a3e63ed84e010f557d8ad
Ludovic Fauvet <etix@videolan.org>
2015-08-07 00:05:19 +0200

    network: split non-geoip functions

--- network/geoip.go
+++ network/geoip.go
@@ -8,7 +8,6 @@ import (
 	"github.com/etix/geoip"
 	. "github.com/etix/mirrorbits/config"
 	"github.com/op/go-logging"
-	"net"
 	"os"
 	"strconv"
 	"strings"
@@ -120,39 +119,4 @@ func (g *GeoIP) IsIPv6(ip string) bool {
 // Return true if the given address is valid
 func (g *GeoIPRecord) IsValid() bool {
 	return g.GeoIPRecord != nil
-}
-
-// Return the IP address of a mirror and return an error
-// if the DNS returns more than one address
-func LookupMirrorIP(host string) (string, error) {
-	addrs, err := net.LookupIP(host)
-	if err != nil {
-		return "", err
-	}
-	// A mirror with multiple IP address is a problem
-	// since we can't determine the exact position of
-	// the server.
-	if len(addrs) > 1 {
-		err = ErrMultipleAddresses
-	}
-
-	return addrs[0].String(), err
-}
-
-// Remove the port from a remote address (x.x.x.x:yyyy)
-func RemoteIpFromAddr(remoteAddr string) string {
-	return remoteAddr[:strings.LastIndex(remoteAddr, ":")]
-}
-
-// Extract the remote IP from an X-Forwarded-For header
-func ExtractRemoteIP(XForwardedFor string) string {
-	addresses := strings.Split(XForwardedFor, ", ")
-	if len(addresses) > 0 {
-		// The left-most address is supposed to be the original client address.
-		// Each successive are added by proxies. In most cases we should probably
-		// take the last address but in case of optimization services this will
-		// probably not work. For now we'll always take the original one.
-		return addresses[0]
-	}
-	return ""
 }
---
+++ network/utils.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2014-2015 Ludovic Fauvet
+// Licensed under the MIT license
+
+package network
+
+import (
+	"net"
+	"strings"
+)
+
+// Return the IP address of a mirror and return an error
+// if the DNS returns more than one address
+func LookupMirrorIP(host string) (string, error) {
+	addrs, err := net.LookupIP(host)
+	if err != nil {
+		return "", err
+	}
+	// A mirror with multiple IP address is a problem
+	// since we can't determine the exact position of
+	// the server.
+	if len(addrs) > 1 {
+		err = ErrMultipleAddresses
+	}
+
+	return addrs[0].String(), err
+}
+
+// Remove the port from a remote address (x.x.x.x:yyyy)
+func RemoteIpFromAddr(remoteAddr string) string {
+	return remoteAddr[:strings.LastIndex(remoteAddr, ":")]
+}
+
+// Extract the remote IP from an X-Forwarded-For header
+func ExtractRemoteIP(XForwardedFor string) string {
+	addresses := strings.Split(XForwardedFor, ", ")
+	if len(addresses) > 0 {
+		// The left-most address is supposed to be the original client address.
+		// Each successive are added by proxies. In most cases we should probably
+		// take the last address but in case of optimization services this will
+		// probably not work. For now we'll always take the original one.
+		return addresses[0]
+	}
+	return ""
+}
