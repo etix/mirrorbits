39ac5f7abfdb61d34734574f8a16bf4a30fe689f
Michael 'PoempelFox' Meier <git@mulder.franken.de>
2018-02-24 11:02:51 +0100

    Make all the font stuff in base.html locally hostable. Note: This will also update the version of fontawesome used in the non-local case from 4.1.0 to 4.7.0 for consistency (because 4.1.0 is not available for download anymore).

--- contrib/localjs/fetchfiles.sh
+++ contrib/localjs/fetchfiles.sh
@@ -35,16 +35,16 @@ getlocalfilename ()
 downloadfile ()
 {
 	curl=`which curl`
-	if [ ${#curl} -gt 4 ] ; then
+	if [ ${#curl} -ge 4 ] ; then
 		$curl --output "$2" "$1"
 		return
 	fi
 	wget=`which wget`
-	if [ ${#wget} -gt 4 ] ; then
+	if [ ${#wget} -ge 4 ] ; then
 		$wget --output-document="$2" "$1"
 		return
 	fi
-	echo "Sorry: Neither curl nor wget were found in path."
+	echo "ERROR: Neither curl nor wget were found in path. Please install either curl or wget."
 	exit 1
 }
 
@@ -63,6 +63,11 @@ if [ ! -d "$localdir" ] ; then
 	exit 1
 fi
 
+unzip=`which unzip`
+if [ ${#unzip} -lt 5 ] ; then
+	echo "ERROR: unzip was not found in path. Please install unzip."
+	exit 1
+fi
 for sf in ${whattofetch[@]}; do
 	lfn="/void/void/void/void/"
 	getlocalfilename "$sf"
@@ -78,4 +83,37 @@ for sf in ${whattofetch[@]}; do
 		downloadfile "$sf" "$tf"
 	fi
 done
+
+# The font stuff is a bit more messy.
+
+# For the Lato font, we rely on a service that's packing the mess into
+# a ZIP file for us.
+if [ ! -e "${localdir}/fonts" ] ; then
+	mkdir -p "${localdir}/fonts"
+fi
+downloadfile "https://google-webfonts-helper.herokuapp.com/api/fonts/lato?download=zip&subsets=latin&variants=900,regular" "${localdir}/fonts/lato-font-900and400.zip"
+# Make sure the webserver cannot access the file for licensing reasons, as
+# that would count as distribution and require proper attribution which
+# we cannot guarantee.
+chmod go-rwx "${localdir}/fonts/lato-font-900and400.zip"
+unzip "${localdir}/fonts/lato-font-900and400.zip" -d "${localdir}/fonts/"
+
+# For fontawesome, we download and unpack the whole ZIP as well.
+# Downloading individual files will not work here, as depending on the
+# browser random other files will be included.
+if [ ! -e "${localdir}/font-awesome" ] ; then
+	mkdir -p "${localdir}/font-awesome"
+fi
+if [ -e "${localdir}/font-awesome/4.7.0" ] ; then
+	# Only download and extract if that directory isn't there yet, as we
+	# have to use a 'mv' command here that would fail if the target already
+	# existed.
+	echo "Note: Skipping font-awesome download and extraction, it seems to be in place already."
+	echo "To force redownload and extraction, remove '${localdir}/font-awesome/4.7.0'"
+else
+	downloadfile "https://fontawesome.com/v4.7.0/assets/font-awesome-4.7.0.zip" "${localdir}/font-awesome-4.7.0.zip"
+	chmod go-rwx "${localdir}/font-awesome-4.7.0.zip"
+	unzip "${localdir}/font-awesome-4.7.0.zip" -d "${localdir}/font-awesome"
+	mv "${localdir}/font-awesome/font-awesome-4.7.0" "${localdir}/font-awesome/4.7.0"
+fi
 
