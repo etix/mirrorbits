c24766127e0b512c2aee7073ad39d6a2ed0790a6
Arnaud Rebillout <arnaudr@kali.org>
2025-03-28 10:20:14 +0700

    redis: Add unit tests for IsAtLeastVersion
    Some of those tests are commented out as they don't pass, due to
    limitations in `parseVersion()`. This function assumes that we compare
    versions that have the same number of components.
    
    Going to improve that in the next commit.

---
+++ database/redis_test.go
@@ -0,0 +1,52 @@
+// Copyright (c) 2025 Arnaud Rebillout
+// Licensed under the MIT license
+
+package database
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsAtLeastVersion(t *testing.T) {
+	testsFalse := [] struct {
+		have string
+		want string
+	} {
+		{"",      "3.2.0"},
+		{"2.0",   "3.2.0"},
+//		{"2.0.0", "3.2"},
+		{"2.0.0", "3.2.0"},
+	}
+	for i, test := range testsFalse {
+		r := Redis{
+			version: test.have,
+		}
+		t.Run(fmt.Sprintf("testsFalse/%d", i), func(t *testing.T) {
+			if r.IsAtLeastVersion(test.want) {
+				t.Errorf("Expected '%s' < '%s'", test.have, test.want)
+			}
+		})
+	}
+
+	testsTrue := [] struct {
+		have string
+		want string
+	} {
+//		{"3.2",   "3.2.0"},
+		{"3.2.0", "3.2.0"},
+//		{"6.0",   "3.2.0"},
+		{"6.0.0", "3.2"},
+		{"6.0.0", "3.2.0"},
+	}
+	for i, test := range testsTrue {
+		r := Redis{
+			version: test.have,
+		}
+		t.Run(fmt.Sprintf("testsTrue/%d", i), func(t *testing.T) {
+			if ! r.IsAtLeastVersion(test.want) {
+				t.Errorf("Expected '%s' >= '%s'", test.have, test.want)
+			}
+		})
+	}
+}
