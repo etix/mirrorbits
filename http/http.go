etix/mirrorbits

commit ed1db9e9cadce5fa183b3dae3c7850beb0452455

http: Get file information earlier, don't wait for selection
This is in preparation for the next commit, where we'll add support for
If-Modified-Since. For that, we need to know the file modTime, so we
need to call GetFileInfo() early enough.

Calling the function earlier doesn't hurt, and doesn't change anything
to the logic of the code. As we can see, before this commit:
- if the function fails, we bail out of Selection() an error, it take us
  to the line `http.Error(w, err.Error(), http.StatusInternalServerError)`
- if the function succeeds, we proceed. That includes the case where a
  file is not found in the Redis DB.

With this commit, this logic is preserved.

Extra notes: (1) `filesystem.NewFileInfo(urlPath)` returns a struct
where _only_ Path is populated, while (2) `cache.GetFileInfo(urlPath)`
returns a struct where _at least_ Path is populated. So calling (1) then
(2) is the same as just calling (2).

http/http.go
@@ -233,7 +233,13 @@ func (h *HTTP) mirrorHandler(w http.ResponseWriter, r *http.Request, ctx *Contex
 		return
 	}
 
-	fileInfo := filesystem.NewFileInfo(urlPath)
+	// Get details about the requested file
+	fileInfo, err := h.cache.GetFileInfo(urlPath)
+	if err != nil {
+		log.Errorf("Error while fetching Fileinfo: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	remoteIP := network.ExtractRemoteIP(r.Header.Get("X-Forwarded-For"))
 	if len(remoteIP) == 0 {

http/selection.go
@@ -29,12 +29,6 @@ type DefaultEngine struct{}
 
 // Selection returns an ordered list of selected mirror, a list of rejected mirrors and and an error code
 func (h DefaultEngine) Selection(ctx *Context, cache *mirrors.Cache, fileInfo *filesystem.FileInfo, clientInfo network.GeoIPRecord) (mlist mirrors.Mirrors, excluded mirrors.Mirrors, err error) {
-	// Get details about the requested file
-	*fileInfo, err = cache.GetFileInfo(fileInfo.Path)
-	if err != nil {
-		return
-	}
-
 	// Prepare and return the list of all potential mirrors
 	mlist, err = cache.GetMirrors(fileInfo.Path, clientInfo)
 	if err != nil {

commit 59d041a2b8f0a54aec40ad03fc50491551bcb015

http: Fix "Internal Server Error" regressions
If the database is not reachable for some reason, mirrorbits must not
return "500 Internal Server Error", instead it must redirect users to
the fallback mirror(s).

This was broken in ed1db9e9cadce5fa183b3dae3c7850beb0452455, here's the
fix.

Closes: #195

http/http.go
@@ -333,12 +333,11 @@ func (h *HTTP) mirrorHandler(w http.ResponseWriter, r *http.Request, ctx *Contex
 		return
 	}
 
-	// Get details about the requested file
+	// Get details about the requested file. Errors are not fatal, and
+	// expected when the database is not ready: fallbacks will handle it.
 	fileInfo, err := h.cache.GetFileInfo(urlPath)
 	if err != nil {
-		log.Errorf("Error while fetching Fileinfo: %s", err.Error())
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		//log.Debugf("Error while fetching Fileinfo: %s", err.Error())
 	}
 
 	if checkIfModifiedSince(r, fileInfo.ModTime) == condFalse {

http/selection.go
@@ -4,6 +4,7 @@
 package http
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"math/rand"
@@ -18,6 +19,10 @@ import (
 	"github.com/etix/mirrorbits/utils"
 )
 
+var (
+	ErrInvalidFileInfo = errors.New("Invalid file info (modtime is zero)")
+)
+
 type mirrorSelection interface {
 	// Selection must return an ordered list of selected mirror,
 	// a list of rejected mirrors and and an error code.
@@ -29,6 +34,12 @@ type DefaultEngine struct{}
 
 // Selection returns an ordered list of selected mirror, a list of rejected mirrors and and an error code
 func (h DefaultEngine) Selection(ctx *Context, cache *mirrors.Cache, fileInfo *filesystem.FileInfo, clientInfo network.GeoIPRecord) (mlist mirrors.Mirrors, excluded mirrors.Mirrors, err error) {
+	// Bail out early if we don't have valid file details
+	if fileInfo.ModTime.IsZero() {
+		err = ErrInvalidFileInfo
+		return
+	}
+
 	// Prepare and return the list of all potential mirrors
 	mlist, err = cache.GetMirrors(fileInfo.Path, clientInfo)
 	if err != nil {